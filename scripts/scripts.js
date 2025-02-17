72f8cfe684b557f21124574cba0e334fcc639255
GallantJoseph <gallant.joseph@outlook.com>
2025-02-17 16:21:01 -0330

    Order Form
    Order form with basic validation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,41 +1,79 @@
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("Welcome!");
-
   document.querySelector("#orderBtn").addEventListener("click", () => {
     submitOrder()
       .then((resolve) => {
-        alert(resolve);
+        displayStatus(resolve, "success");
       })
       .catch((reject) => {
-        alert(reject);
+        displayStatus(reject, "fail");
       });
   });
 
   function submitOrder() {
     return new Promise((resolve, reject) => {
-      //resolve("Success!");
-
       try {
         validateOrderFields();
+
+        resolve("Order Submitted");
       } catch (error) {
-        alert(error);
-        reject("Failed!");
+        reject(error.message);
       }
     });
   }
 
   function validateOrderFields() {
-    let name = document.querySelector("#").value
+    let name = document.querySelector("#userFNameTextBox").value.trim();
+    let phone = document.querySelector("#userPhoneTextBox").value.trim();
+    let email = document.querySelector("#userEmailTextBox").value.trim();
+    let address = document.querySelector("#userAddressTextBox").value.trim();
+    let error = "";
+
+    let phoneRegex = /\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{4}/;
+    let emailRegex = /\w+\@\w+(\.\w{2,})+/;
+    let addressRegex = /\d+(\-?[\d\w]+)*[\,\s]\s?\w+/; // Accepts a: "#(-#*/X*), Street Name" Format, (i.e. 123-A, Main Street)
+
     // Validate Name
+    if (name === "") {
+      error += "Enter Your Name<br />";
+    }
 
     // Validate Phone Number
+    if (phone === "") {
+      error += "Enter Your Phone Number<br />";
+    } else if (!phoneRegex.test(phone)) {
+      error += "Invalid Phone Number<br />";
+    }
 
     // Validate Email
+    if (email === "") {
+      error += "Enter Your Email<br />";
+    } else if (!emailRegex.test(email)) {
+      error += "Invalid Email<br />";
+    }
 
     // Validate Address
+    if (address === "") {
+      error += "Enter Your Address<br />";
+    } else if (!addressRegex.test(address)) {
+      error += "Invalid Address<br />";
+    }
 
     // Validate Order Type
+    if (
+      !document.querySelector("#pickUpRdio").checked &&
+      !document.querySelector("#deliveryRdio").checked
+    ) {
+      error += "Select an Order Type (Pickup or Delivery)<br />";
+    }
 
-    throw new Error("Test");
+    if (error !== "") {
+      throw new Error(error);
+    }
+  }
+
+  function displayStatus(message, className) {
+    let statusText = document.querySelector("#statusText");
+    statusText.innerHTML = message;
+    statusText.className = className;
   }
 });
